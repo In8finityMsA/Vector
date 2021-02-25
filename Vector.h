4bba45283d27383825a20e7c2e694e792bd75338
In8finity <wintry3@gmail.com>
2021-02-25 14:48:47 +0300

    Adjustments to copy and move constructor/assigment. Added push and pop_back

--- Vector.h
+++ Vector.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <chrono>
 #include <stdexcept>
+#include <type_traits>
 
 #ifndef TEMPLATE_VECTOR_H
 #define TEMPLATE_VECTOR_H
@@ -16,23 +17,27 @@ public:
 
     Vector() : size_(0), capacity_(1) {
         data_ = new ContainerType[capacity_];
+        std::cout << "Constructor: empty" << std::endl;
     }
     explicit Vector(std::size_t size) : size_(size), capacity_(size) {
         data_ = new ContainerType[capacity_]();
+        std::cout << "Constructor: n" << std::endl;
     }
     Vector(std::size_t size, const ContainerType& elem) : size_(size), capacity_(size) {
         data_ = new ContainerType[capacity_];
         for (int i = 0; i < size_; i++) {
             data_[i] = elem;
         }
+        std::cout << "Constructor: n, elem" << std::endl;
     }
 
     Vector(std::initializer_list<ContainerType> il) : size_(il.size()), capacity_(il.size()) {
-        data_ = new int[capacity_];
+        data_ = new ContainerType[capacity_];
         int i = 0;
         for (auto item : il) {
             data_[i++] = item;
         }
+        std::cout << "Constructor: initializer list" << std::endl;
     };
     Vector& operator= (std::initializer_list<ContainerType> il){
         size_ = il.size();
@@ -45,48 +50,100 @@ public:
         for (auto item : il) {
             data_[i++] = item;
         }
+        std::cout << "Assign: = initializer list" << std::endl;
         return *this;
     };
 
     Vector(const Vector<ContainerType>& vector) : size_(vector.size()), capacity_(vector.size()) {
         data_ = new ContainerType[capacity_];
 
-        for (int i = 0; i < vector.size(); i++) {
-            data_[i] = vector[i];
+        if (std::is_integral<ContainerType>::value) {
+            memcpy(data_, vector.data_, size_ * sizeof(ContainerType));
+            std::cout << "Constructor: = copy integral" << std::endl;
+        }
+        else {
+            for (int i = 0; i < vector.size(); i++) {
+                data_[i] = vector[i];
+            }
+            std::cout << "Constructor: = copy not integral" << std::endl;
         }
     }
     Vector<ContainerType>& operator = (const Vector<ContainerType>& vector) {
-        if (this == &vector) {
-            return  *this;
-        }
-        size_ = vector.size();
-        capacity_ = vector.size();
+        if (this != &vector) {
+            size_ = vector.size();
+            capacity_ = vector.size();
 
-        delete[] data_;
-        data_ = new ContainerType[capacity_];
+            delete[] data_;
+            data_ = new ContainerType[capacity_];
 
-        for (int i = 0; i < vector.size(); i++) {
-            data_[i] = vector[i];
+            if (std::is_integral<ContainerType>::value) {
+                memcpy(data_, vector.data_, size_ * sizeof(ContainerType));
+                std::cout << "Assign: = copy integral" << std::endl;
+            } else {
+                auto start = std::chrono::steady_clock::now();
+                for (int i = 0; i < vector.size(); i++) {
+                    data_[i] = vector[i];
+                }
+                std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count() << std::endl;
+                std::cout << "Assign: = copy not integral" << std::endl;
+            }
         }
 
         return *this;
     }
+
     Vector(Vector<ContainerType>&& vector) noexcept : size_(vector.size()), capacity_(vector.size()) {
         data_ = vector.data_;
+        vector.data_ = nullptr;
+        vector.size_ = 0;
+        vector.capacity_ = 0;
+        std::cout << "Constructor: move" << std::endl;
     }
     Vector<ContainerType>& operator = (Vector<ContainerType>&& vector) noexcept {
-        size_ = vector.size();
-        capacity_ = vector.size();
-        delete[] data_;
+        if (this != &vector) {
+            size_ = vector.size();
+            capacity_ = vector.size();
+            delete[] data_;
 
-        data_ = vector.data_;
+            data_ = vector.data_;
+            vector.data_ = nullptr;
+            vector.size_ = 0;
+            vector.capacity_ = 0;
+            std::cout << "Assign: = move" << std::endl;
+        }
 
         return *this;
     }
 
-    void push_back(const ContainerType& elem);
-    void push_back(ContainerType&& elem);
-    void pop_back();
+    void reallocate() {
+        auto tempPtr = data_;
+        data_ = new ContainerType[capacity_ *= 2];
+        for (int i = 0; i < size_; i++) {
+            data_[i] = tempPtr[i];
+        }
+        delete[] tempPtr;
+    }
+    void push_back(const ContainerType& elem) {
+        if (size_ >= capacity_) {
+            reallocate();
+            std::cout << "Realloc finished" << std::endl;
+        }
+        if (data_ != nullptr)
+            data_[size_++] = elem;
+        else throw std::runtime_error("Null pointer exception!");
+    }
+    void push_back(ContainerType&& elem) {
+        if (size_ >= capacity_) {
+            reallocate();
+        }
+        if (data_ != nullptr) {
+            data_[size_++] = std::move(elem);
+        }
+        else throw std::runtime_error("Null pointer exception!");
+    };
+    void pop_back() {
+        size_--;
+    };
     void insert(std::size_t index, const ContainerType& elem);
 
     void erase(std::size_t index);
--- main.cpp
+++ main.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
 #include "Vector.h"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main() {
 
+    const int n = 100000;
     try {
-        Vector<int> intv(200000000, 1729);
+        Vector<int> intv0{1,3,5,6,2}; //Constr init list
+
+        ///Pushback check
+        Vector<int> intv1; //Constr empty
+        for (int i = 0; i < n; i++) {
+            intv1.push_back(i); //Copy pushback
+        }
+        ///Massive copy
+        Vector<int> intv2(intv1); //Constr copy
+        cout << intv2[0] << endl;
+
+        ///Pushback check nonIntegral
+        Vector<string> st1; //Constr empty
+        for (int i = 0; i < n; i++) {
+            st1.push_back(std::to_string(i)); //Move pushback
+        }
+        ///Massive copy nonIntegral
+        Vector<string> st2(st1); //Constr copy
+        cout << st2[289] << endl;
+        /*for (int i = 0; i < intv2.size(); i++) {
+            std::cout << intv2[i] << endl;
+        }*/
+
+        ///Move check
+        Vector<int> intv3 = {1,3,4}; //Constr init list
+        intv3 = Vector<int>(5, 167); //Assign move + n,elem
+        auto intv4(std::move(intv3)); //Constr move
+        intv4 = std::move(intv4); //Assign move (self)
+        cout << intv4[0] << endl;
+        //cout << "Size intv3: " << intv3.size() << endl;
+
+        for (int i = 1; i < intv3.size(); i++) {
+            cout << "Size intv3: " << intv3.size() << " " << i << endl;
+            std::cout << intv3[i] << endl;
+            intv3[i-1] = intv3[i];
+            intv3[3] = 300;
+        }
+        std::cout << endl;
+        for (int i = 0; i < intv3.size(); i++) {
+            cout << "Size intv3: " << intv3.size() << endl;
+            std::cout << intv3[i] << endl;
+        }
+
+        ///Check of not integral types
+        /*vector<string> vec0{"fhh","hfhhh","ghfdbfd","rtnnbt","bfbfbngfnn"};
+        vector<string> vec1{"fhffh","hfjhhhh","gdbfd","rtbt","bfbhjhjgfnn"};
+        vector<string> vec2{"fdhh","hfh","ghfdghjhbfd","rtnnbt","bfbffnn"};
+        Vector<vector<string>> strv{vec0, vec1, vec2}; //init list
+        Vector<vector<string>> strv1(strv); //
+        for (int i = 0; i < strv1.size(); i++) {
+            for (int j = 0; j < strv1[i].size(); j++) {
+                std::cout << strv1[i][j] << endl;
+            }
+            cout << endl;
+        }
+        strv[0][0] = "hello";
+        cout << strv[0][0] << endl;
+        cout << vec0[0] << endl;*/
     }
     catch (std::exception& e) {
         std::cout << e.what() << std::endl;
@@ -13,6 +75,6 @@ int main() {
         std::cout << "Exception";
     }
 
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
